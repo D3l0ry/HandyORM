1ffe72fa8d1479a3910fde90684ad93c3eee2077
Даниил Шантарин <d3l0ry@yandex.ru>
2022-12-08 13:19:06 +0500

    Изменены имена классов и исправлены ошибки

---
+++ Converter/DataConverter.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using Handy.Extensions;
+
+namespace Handy
+{
+    /// <summary>
+    /// Класс для конвертации объектов из SqlDataReader
+    /// </summary>
+    internal class DataConverter
+    {
+        private readonly Type _ObjectType;
+        private readonly PropertyInfo[] _ObjectProperties;
+
+        /// <summary>
+        /// Инициализатор
+        /// </summary>
+        /// <param name="type">Тип объекта, к которому нужно привести объекты из SqlDataReader</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public DataConverter(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            _ObjectType = type;
+            _ObjectProperties = type.GetProperties();
+        }
+
+        /// <summary>
+        /// Тип объекта
+        /// </summary>
+        protected Type ObjectType => _ObjectType;
+
+        /// <summary>
+        /// Конвертирует поля из строки SqlDataReader в объекты выбранного типа
+        /// </summary>
+        /// <param name="dataReader"></param>
+        /// <returns></returns>
+        protected virtual object GetInternalObject(DbDataReader dataReader)
+        {
+            if (dataReader.FieldCount == 1)
+            {
+                object value = dataReader.GetValue(0);
+                Type valueType = value.GetType();
+
+                if (valueType == _ObjectType)
+                {
+                    return value;
+                }
+            }
+
+            object table = Activator.CreateInstance(_ObjectType);
+
+            foreach (PropertyInfo currentProperty in _ObjectProperties)
+            {
+                currentProperty.SetDataReaderValue(table, dataReader);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Получение массива объектов из таблицы
+        /// </summary>
+        /// <param name="dataReader"></param>
+        /// <returns></returns>
+        public virtual IEnumerable GetObjects(DbDataReader dataReader)
+        {
+            IList list = (IList)Activator
+                .CreateInstance(typeof(List<>)
+                .MakeGenericType(_ObjectType));
+
+            using (dataReader)
+            {
+                while (dataReader.Read())
+                {
+                    object newObject = GetInternalObject(dataReader);
+
+                    list.Add(newObject);
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Получение объекта из таблицы
+        /// </summary>
+        /// <param name="dataReader"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public virtual object GetObject(DbDataReader dataReader)
+        {
+            using (dataReader)
+            {
+                if (dataReader.Read())
+                {
+                    return GetInternalObject(dataReader);
+                }
+
+                if (_ObjectType.IsValueType)
+                {
+                    return Activator.CreateInstance(_ObjectType);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Converter/TableConverter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using Handy.Extensions;
+using Handy.TableInteractions;
+
+namespace Handy.Converter
+{
+    /// <summary>
+    /// Класс для конвертации объектов из SqlDataReader в тип определяющий таблицу базы данных
+    /// </summary>
+    internal class TableConverter : DataConverter
+    {
+        private readonly DbConnection _CurrentContextConnection;
+        private readonly TableProperties _TableProperties;
+
+        internal TableConverter(Type tableType, DbConnection connection) : base(tableType)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            TableQueryCreator tableQueryCreator = TableQueryCreator.GetInstance(tableType);
+
+            _CurrentContextConnection = connection;
+            _TableProperties = tableQueryCreator.PropertyQueryCreator;
+        }
+
+        /// <summary>
+        /// Получение объектов из внешней таблицы
+        /// </summary>
+        /// <param name="mainTable"></param>
+        /// <param name="property"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void SetCreatedInstanceForeignTable(object mainTable, in KeyValuePair<PropertyInfo, ColumnAttribute> property)
+        {
+            Type foreignTableType = typeof(ForeignTable<>);
+            PropertyInfo selectedProperty = property.Key;
+            Type selectedPropertyType = selectedProperty.PropertyType;
+            ColumnAttribute selectedPropertyColumn = property.Value;
+
+            if (string.IsNullOrWhiteSpace(selectedPropertyColumn.ForeignKeyName))
+            {
+                throw new NullReferenceException($"Не указан внешний ключ для {selectedProperty.Name}");
+            }
+
+            if (!selectedPropertyType.IsGenericType || selectedPropertyType.GetGenericTypeDefinition() != foreignTableType)
+            {
+                throw new ArgumentException($"Свойство не является типом {foreignTableType.Name}");
+            }
+
+            PropertyInfo mainTableForeignKeyProperty = _TableProperties
+                .GetProperty(selectedPropertyColumn.ForeignKeyName).Key;
+
+            ConstructorInfo foreignTableConstructor = selectedPropertyType
+                .GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[]
+                    { typeof(object), typeof(PropertyInfo), typeof(DbConnection) }, null);
+
+            object newForeignTableInstance = foreignTableConstructor
+                .Invoke(new object[] { mainTable, mainTableForeignKeyProperty, _CurrentContextConnection });
+
+            selectedProperty.SetValue(mainTable, newForeignTableInstance);
+        }
+
+        /// <summary>
+        /// Получение объекта из таблицы
+        /// </summary>
+        /// <param name="dataReader"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override object GetInternalObject(DbDataReader dataReader)
+        {
+            object table = Activator.CreateInstance(ObjectType);
+
+            foreach (KeyValuePair<PropertyInfo, ColumnAttribute> currentProperty in _TableProperties)
+            {
+                if (currentProperty.Value.IsTable)
+                {
+                    SetCreatedInstanceForeignTable(table, currentProperty);
+
+                    continue;
+                }
+
+                currentProperty.SetDataReaderValue(table, dataReader);
+            }
+
+            return table;
+        }
+    }
+}
