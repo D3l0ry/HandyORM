76131653a888f34fce77c7b94679c94aa4513062
Даниил Шантарин <D3l0ry@yandex.ru>
2023-09-18 16:50:57 +0500

    Добавлен атрибут для работы со внешними таблицами

---
+++ Attributes/ForeignTableAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Handy.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ForeignTableAttribute : Attribute
+    {
+        public string ThisKey { get; set; }
+
+        public string OtherKey { get; set; }
+    }
+}
